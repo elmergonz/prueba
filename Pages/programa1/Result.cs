53dedd8a3e88ddee0827eb39060d0f97ac8d80f0
elmergonz <elmer.go@outlook.com>
2021-03-16 01:30:08 -0400

    Borrar todo

--- Pages/programa1/Result.cs
+++
@@ -1,9 +0,0 @@
-namespace tarea2.Pages.programa1
-{
-    public class Result
-    {
-        public int Espacios { get; set; }
-        public int Vocales { get; set; }
-        public int Consonantes { get; set; }
-    }
-}
